6bc26d260a39f67f59644fca07924efb84a8e0bc
rizfirman <rizfirman@gmail.com>
2023-05-21 21:51:16 +0700

    add open/close sidebar feature

--- src/components/LayoutDashboard/index.js
+++ src/components/LayoutDashboard/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CatraLogo from '../../assets/icons/catra-logo.svg';
 import { Link, NavLink } from 'react-router-dom';
 import DashboardLogo from '../../assets/icons/dashboard-icon.svg';
@@ -8,18 +8,31 @@ import DataSubsIcon from '../../assets/icons/data-subs-icon.svg';
 import OnlineShopIcon from '../../assets/icons/online-shop-icon.svg';
 import NonTunaiIcon from '../../assets/icons/non-tunai-icon.svg';
 import ConfigIcon from '../../assets/icons/config-icon.svg';
+import sideBarOpen from '../../assets/icons/sidebar-open.svg';
+import sideBarClose from '../../assets/icons/sidebar-close.svg';
 
 const Layout = ({ children, active }) => {
+  const [isShowSideBar, setIsShowSideBar] = useState(false);
+  const onClickSideBar = () => {
+    setIsShowSideBar(!isShowSideBar);
+  }
   return (
     <div>
       <div className="mx-auto  grid grid-cols-12">
-        <header className="col-span-12 p-5 shadow-lg ">
+        <header className="col-span-12 p-5 shadow-lg flex items-center">
           <div>
-            <img src={CatraLogo} alt="Catra Logo" />
+            <img src={CatraLogo} alt="Catra Logo" className='mr-3'/>
+          </div>
+          <div>
+            {isShowSideBar ? (
+              <img src={sideBarOpen} alt="sidebar open" onClick={() => onClickSideBar()} />
+            ) : (
+              <img src={sideBarClose} alt="sidebar close" onClick={() => onClickSideBar()}  />
+            )}
           </div>
         </header>
 
-        <aside className="col-span-2 border border-r-2">
+        <aside className={isShowSideBar ? "col-span-2 border border-r-2" : "hidden"}>
           <NavLink to="/dashboard" className="flex p-5 hover:bg-gray-200">
             <img src={DashboardLogo} alt="Dashboard Logo" className="mr-2" />
             <p className="text-[16px] font-[500] text-[#6B7280] font-sans">Dashboard</p>
@@ -52,7 +65,7 @@ const Layout = ({ children, active }) => {
           </Link>
         </aside>
 
-        <main class="col-span-10 h-[100vh]  p-10 overflow-scroll">{children}</main>
+        <main className={isShowSideBar ? "col-span-10 h-[100vh]  p-10 overflow-scroll" : "col-span-12 h-[100vh]  p-10 overflow-scroll"}>{children}</main>
       </div>
     </div>
   );
